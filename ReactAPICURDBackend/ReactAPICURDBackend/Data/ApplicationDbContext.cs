fcdad2a5768d1fabca86bfb81426b61c37c8df74
Tamimkhan7 <mdtamimkhan842@gmail.com>
2025-10-17 02:24:19 +0600

    added full-stack api project

---
+++ ReactAPICURDBackend/ReactAPICURDBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactAPICURDBackend.Models;
+
+namespace ReactAPICURDBackend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> students { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=.; initial catalog=ibs; User Id=sa; password123;  TrustServerCertificate = True;");
+        //}
+
+    }
+
+}
